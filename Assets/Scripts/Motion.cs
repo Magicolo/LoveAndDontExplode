bb9dc8d246e6e6623090deb3fd3556af1c24219d
Magicolo <straty91@hotmail.com>
2016-05-20 13:09:44 -0400

    A player is a moving

---
+++ Assets/Scripts/Motion.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using Pseudo;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Motion : MotionBase
+{
+	Rigidbody2D body;
+
+	void Awake()
+	{
+		body = GetComponent<Rigidbody2D>();
+	}
+
+	public override void Move(Vector2 motion)
+	{
+		body.AddForce(motion, ForceMode2D.Impulse);
+	}
+}
