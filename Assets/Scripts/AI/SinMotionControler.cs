360750de4030ece64bb0ba2934fe9edb6a5a65d3
Dracir <rail.rich@gmail.com>
2016-05-20 18:18:20 -0400

    asd

---
+++ Assets/Scripts/AI/SinMotionControler.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using Pseudo;
+
+[RequireComponent(typeof(TimeComponent))]
+public class SinMotionControler : ControllerBase
+{
+	public MotionBase Motion;
+
+	float t;
+	public float Frequency = 1;
+	public float Amplitude = 1;
+	public float Offset;
+
+
+	void FixedUpdate()
+	{
+		var y = (float)(Amplitude * Math.Sin(Frequency * t + Offset));
+		var direction = new Vector2(1, y);
+		t += GetComponent<TimeComponent>().DeltaTime;
+
+		if (direction != Vector2.zero)
+		{
+			Motion.Move(direction);
+		}
+	}
+}
--- Assets/Scripts/LeveLevents/Events/SpawnLevelEvent.cs
+++ Assets/Scripts/LeveLevents/Events/SpawnLevelEvent.cs
@@ -6,22 +6,50 @@ using System.Collections.Generic;
 using Pseudo;
 using Pseudo.Injection;
 
+[RequireComponent(typeof(TimeComponent))]
 public class SpawnLevelEvent : ILevelEvent
 {
 
 	[Inject(Cameras.Main)]
 	Camera Cam;
 
-	[Slider(0, 1)]
+	[Slider(0, 1), Tooltip("Location Spawnned par rapport à L'écran. 0 = gauche, 1 = à droite.")]
 	public float RatioXStart;
+
+	[Min(0), Tooltip("Spawn every X")]
+	public float SpawnFrequency = 1;
+
+	public int SpawnQuantity = 1;
+
+	int spawned;
+	float nextSpawn = -1;
 
 	public GameObject PrefabToSpawn;
 
 	internal override void Activate()
 	{
+		Spawn();
+	}
+
+	void Update()
+	{
+		if (GetComponent<TimeComponent>().Time >= nextSpawn)
+		{
+			Spawn();
+		}
+	}
+
+	private void Spawn()
+	{
+		float levelHeight = GetComponentInParent<LevelEvents>().LevelHeight;
 		GameObject go = GameObject.Instantiate(PrefabToSpawn);
-		Vector3 p = Cam.ViewportToWorldPoint(new Vector3(RatioXStart, transform.localPosition.y / 100,0));
+		Vector3 p = Cam.ViewportToWorldPoint(new Vector3(RatioXStart, transform.localPosition.y / levelHeight, -Cam.transform.position.z));
+		PDebug.Log(new Vector3(RatioXStart, transform.localPosition.y / levelHeight, 0), p);
 		// TODO offsetSelon le sprite;
 		go.transform.position = p;
+		nextSpawn = GetComponent<TimeComponent>().Time + SpawnFrequency;
+		spawned++;
+		if (spawned >= SpawnQuantity)
+			enabled = false;
 	}
 }
