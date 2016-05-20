46011aaa6f598aa970e1a0d0fbb5135f029c7da3
Dracir <rail.rich@gmail.com>
2016-05-20 19:39:24 -0400

    asd

--- Assets/Scripts/LevelEvents/Events/FreezeTimelineUntilEnemisKilled.cs
+++ Assets/Scripts/LevelEvents/Events/FreezeTimelineUntilEnemisKilled.cs
@@ -7,11 +7,12 @@ using Pseudo;
 using Pseudo.Editor.Internal;
 
 [RequireComponent(typeof(LevelEventActivator), typeof(Rigidbody2D))]
-public class FreezeTielineUntilTriggerHitFreezeTielineUntilTriggerHitFreezeTimelineUntilEnemisKilled : ILevelEvent
+public class FreezeTimelineUntilEnemisKilled : ILevelEvent
 {
 
 	FreezeMotion freeze;
-	bool allDead { get { return GameObject.FindGameObjectWithTag("Enemis") == null; } }
+	public int UntilRemaingXEnemis;
+	bool allDead { get { return GameObject.FindGameObjectsWithTag("Enemis").Length <= UntilRemaingXEnemis; } }
 
 	internal override void Activate()
 	{
@@ -35,5 +36,12 @@ public class FreezeTielineUntilTriggerHitFreezeTielineUntilTriggerHitFreezeTimel
 		Gizmos.DrawLine(down, down + new Vector3(0, LevelHeight));
 		Gizmos.DrawSphere(down, 1);
 		Gizmos.DrawSphere(down + new Vector3(0, LevelHeight), 1);
+
+		if (freeze != null)
+		{
+			int nbEnemis = GameObject.FindGameObjectsWithTag("Enemis").Length;
+			DrawUtility.DrawText(down + new Vector3(0, -2), nbEnemis + " > " + UntilRemaingXEnemis, Color.red);
+		}
+
 	}
 }
