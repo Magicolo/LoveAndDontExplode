7eb0be5104075d2f743e00bd2ffd14fa74ba7e3f
Dracir <rail.rich@gmail.com>
2016-05-20 15:23:21 -0400

    asd

---
+++ Assets/Scripts/World/LevelEventActivator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using Pseudo;
+
+[RequireComponent(typeof(TimeComponent))]
+public class LevelEventActivator : MonoBehaviour
+{
+
+	
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		var worldCollider = other.GetComponent<WorldCollider>();
+		if (worldCollider)
+		{
+			
+		}
+	}
+}
---
+++ Assets/Scripts/World/WorldCollider.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using Pseudo;
+
+[RequireComponent(typeof(Time))]
+public class WorldCollider : MonoBehaviour
+{
+	public float Speed;
+
+	void Update() 
+	{
+		TimeComponent time = GetComponent<TimeComponent>();
+		transform.Translate(Speed * time.DeltaTime,0,0);
+	}
+}
