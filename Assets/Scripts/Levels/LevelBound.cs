900bf3e0ac6d4b434fa5ee96d196971556bcfb21
Dracir <rail.rich@gmail.com>
2016-05-22 12:00:23 -0400

    LevelBound

---
+++ Assets/Scripts/Levels/LevelBound.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using Pseudo;
+
+public class LevelBound : MonoBehaviour
+{
+	public Camera Cam;
+
+	void OnDrawGizmos()
+	{
+		if (Cam == null)
+			return;
+
+		Vector3 bl = Cam.WorldBottomLeft();
+		Vector3 br = Cam.WorldBottomRight();
+		Vector3 tl = Cam.WorldTopLeft();
+		Vector3 tr = Cam.WorldTopRight();
+
+		Gizmos.color = new Color(1, 0.8f, 0);
+
+		Gizmos.DrawLine(bl, br);
+		Gizmos.DrawLine(tl, tr);
+		Gizmos.DrawLine(tl, bl);
+		Gizmos.DrawLine(tr, br);
+	}
+
+}
