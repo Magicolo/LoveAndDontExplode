2e6edc0edb0eca61210f3a92d9ae76e50503bf87
Dracir <rail.rich@gmail.com>
2016-05-23 08:45:10 -0400

    ToggleControllerAfterTime.cs

---
+++ Assets/Scripts/AI/Motion/ToggleControllerAfterTime.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using Pseudo;
+
+public class ToggleControllerAfterTime : MonoBehaviour
+{
+	public ControllerBase Controller;
+	public TimeComponent Time;
+
+	public float DeactivateTime;
+
+	float t;
+
+	void Start()
+	{
+		t = Time.Time + DeactivateTime;
+	}
+
+	void Update()
+	{
+		if (t > Time.Time)
+		{
+			Controller.enabled = !Controller.enabled;
+		}
+	}
+}
