f477e974103f60cb835a56f15849d37dabcd945c
Magicolo <straty91@hotmail.com>
2016-05-22 23:19:06 -0400

    FadeSpeed for colorizer GameLoop

--- Assets/Scripts/Levels/LevelManager.cs
+++ Assets/Scripts/Levels/LevelManager.cs
@@ -4,14 +4,21 @@ using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using Pseudo;
+using Pseudo.Injection;
 
 public class LevelManager : PMonoBehaviour
 {
 	public Damageable[] Ships;
 	public Lane[] Lanes;
 
+	[Inject]
+	IGameManager gameManager = null;
+
 	void Update()
 	{
 		bool allDead = Array.TrueForAll(Ships, s => s == null || !s.Alive);
+
+		if (allDead)
+			gameManager.ReloadScene();
 	}
 }
