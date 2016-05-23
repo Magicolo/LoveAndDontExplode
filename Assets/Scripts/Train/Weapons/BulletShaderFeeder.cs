fc70c2ab4153869efa2cc35af8710d2ddc6c7d29
Dracir <rail.rich@gmail.com>
2016-05-22 23:17:36 -0400

    Ammo reaminngin Shaders.meta

---
+++ Assets/Scripts/Train/Weapons/BulletShaderFeeder.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using Pseudo;
+
+public class BulletShaderFeeder : MonoBehaviour
+{
+	private Material Mat;
+	public WeaponBase Weapon;
+
+	void Start()
+	{
+		Mat = GetComponent<MeshRenderer>().material;
+	}
+	void Update()
+	{
+		Mat.SetFloat("_CoolDownRatio", Weapon.getCoolDownRatio());
+		Mat.SetFloat("_AmmoRatio", Weapon.getAmmoRatio());
+	}
+}
